19a0f314326d82dd611f021ccd9f0b62b003838e
davenrai@gmail.com <davenrai@gmail.com>
2022-02-09 23:38:15 -0500

    Folder Structure tooled and backend added

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(cors());
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const spotifyApi = new SpotifyWebApi(/* credentials */);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// app login
+app.get("/login", (req, res) => {
+  let state = generateRandomString(16);
+  let scopes = "user-read-private user-read-email";
+  let authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+  res.redirect(authorizeURL);
+});
+
+// app callback
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+var generateRandomString = function (length) {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};

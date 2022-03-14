55265ca7e5f4b5dd5477eb80a374c2caea6a8b70
davenrai@gmail.com <davenrai@gmail.com>
2022-03-14 01:07:37 -0400

    Removed pagination for searched albums

---
+++ frontend/src/components/UserReviews.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function UserReviews() {
+  // make a call to db to get all ids of reviews from the user.
+  // display a mini album detail of each.
+  return <div>UserReviews</div>;
+}

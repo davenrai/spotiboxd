a2fbfcc17dbd67c490078fd1fcc0457810d0b7fc
davenrai@gmail.com <davenrai@gmail.com>
2022-03-31 00:13:03 -0400

    move backend to parent directory

--- db/index.js
+++ db/index.js
@@ -3,11 +3,11 @@ const Pool = require("pg").Pool;
 //  pool object you created above will allow you to
 // query into the database that it’s connected to
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
   database: process.env.DATABASE,
-  password: process.env.DBPASSWORD,
-  port: 5432,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 module.exports = {
